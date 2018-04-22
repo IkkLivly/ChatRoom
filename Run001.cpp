144e354cffa92839a6a45c4698af8a1e06665f3d
IkkLivly <33955299+IkkLivly@users.noreply.github.com>
2018-04-22 16:24:02 -0400

    Add files via upload

---
+++ Run001.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+
+{
+	
+cout << "Hello There!"<< endl;
+cout << "Press Enter To Continue" << endl
+cin.ignore();
+cin.get();
+return0;
+}//end main
+
